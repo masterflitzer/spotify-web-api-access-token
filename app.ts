1831ff26c415505de78f3c7cf754849f5de71ec8
masterflitzer <60170948+masterflitzer@users.noreply.github.com>
2023-08-18 14:21:31 +0200

    migrate from express to koa

--- app.ts
+++ app.ts
@@ -3,7 +3,8 @@ import * as crypto from "node:crypto";
 import * as querystring from "node:querystring";
 
 import config from "./config.json" assert { type: "json" };
-import express, { Request, Response } from "express";
+import Koa, { Context, Next } from "koa";
+import Router from "koa-router";
 
 function generateRandomString(length: number): string {
     const characters =
@@ -28,6 +29,18 @@ function authorizationBasicClientBase64(): string {
     return Buffer.from(`${client_id}:${client_secret}`).toString("base64");
 }
 
+class Result {
+    success: boolean;
+    message: string | null;
+    data: Data | null;
+
+    constructor(success: boolean, message: string | null, data: Data | null) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+}
+
 type Data = {
     access_token: string | null;
     token_type: string | null;
@@ -47,10 +60,22 @@ const data: Data = {
 let code: string;
 let state: string;
 
-const app = express();
+const app = new Koa();
+const router = new Router();
 
-app.get("/", async (_req: Request, res: Response) => {
-    const resultResponse = await fetch(
+app.use(async (ctx: Context, next: Next) => {
+    try {
+        await next();
+    } catch (error) {
+        console.error(error);
+        ctx.status = 500;
+        ctx.body = new Result(false, null, null);
+        return;
+    }
+});
+
+router.get("/", async (ctx: Context) => {
+    const response = await fetch(
         new URL("https://api.spotify.com/v1/users/spotify"),
         {
             cache: "no-cache",
@@ -60,18 +85,19 @@ app.get("/", async (_req: Request, res: Response) => {
             method: "GET",
         }
     );
-    const result = await resultResponse.json();
+    const result = await response.json();
 
     if (result.error != null) {
-        return res.redirect("/login");
+        return ctx.redirect("/login");
     }
 
-    return res.send(data);
+    ctx.body = new Result(true, null, data);
+    return;
 });
 
-app.get("/login", (_req: Request, res: Response) => {
+router.get("/login", (ctx: Context) => {
     state = generateRandomString(16);
-    return res.redirect(
+    return ctx.redirect(
         "https://accounts.spotify.com/authorize?" +
             querystring.stringify({
                 client_id: config.client_id,
@@ -84,34 +110,38 @@ app.get("/login", (_req: Request, res: Response) => {
     );
 });
 
-app.get("/callback", (req: Request, res: Response) => {
-    if (req.query.state != state) {
-        return res
-            .status(400)
-            .send(
-                "The generated state doesn't match the received one, watch out for cross-site request forgery attacks!"
-            );
+router.get("/callback", (ctx: Context) => {
+    if (ctx.query.state != state) {
+        ctx.status = 400;
+        ctx.body = new Result(
+            false,
+            "The generated state doesn't match the received one, watch out for cross-site request forgery attacks!",
+            null
+        );
+        return;
     }
 
-    if (req.query.error != null) {
-        return res.status(400).send(req.query.error);
+    if (ctx.query.error != null) {
+        ctx.status = 400;
+        ctx.body = new Result(false, ctx.query.error as string, null);
+        return;
     }
 
-    code = req.query.code as string;
+    code = ctx.query.code as string;
 
-    return res.redirect("/access");
+    return ctx.redirect("/access");
 });
 
-app.get("/access", async (_req: Request, res: Response) => {
+router.get("/access", async (ctx: Context) => {
     const body = new URLSearchParams();
     body.set("grant_type", "authorization_code");
     body.set("code", code);
     body.set("redirect_uri", redirect_uri());
 
-    const resultResponse = await fetch(
+    const response = await fetch(
         new URL("https://accounts.spotify.com/api/token"),
         {
-            body: body,
+            body,
             cache: "no-cache",
             headers: new Headers({
                 Authorization: `Basic ${authorizationBasicClientBase64()}`,
@@ -120,7 +150,7 @@ app.get("/access", async (_req: Request, res: Response) => {
             method: "POST",
         }
     );
-    const result = await resultResponse.json();
+    const result: Data = await response.json();
 
     data.access_token = result.access_token;
     data.token_type = result.token_type;
@@ -128,22 +158,22 @@ app.get("/access", async (_req: Request, res: Response) => {
     data.expires_in = result.expires_in;
     data.refresh_token = result.refresh_token;
 
-    return res.redirect("/");
+    return ctx.redirect("/");
 });
 
-app.get("/refresh", async (_req: Request, res: Response) => {
+router.get("/refresh", async (ctx: Context) => {
     if (data.refresh_token == null) {
-        return res.redirect("/");
+        return ctx.redirect("/");
     }
 
     const body = new URLSearchParams();
     body.set("grant_type", "authorization_code");
     body.set("refresh_token", data.refresh_token);
 
-    const resultResponse = await fetch(
+    const response = await fetch(
         new URL("https://accounts.spotify.com/api/token"),
         {
-            body: body,
+            body,
             cache: "no-cache",
             headers: new Headers({
                 Authorization: `Basic ${authorizationBasicClientBase64()}`,
@@ -152,18 +182,22 @@ app.get("/refresh", async (_req: Request, res: Response) => {
             method: "POST",
         }
     );
-    const result = await resultResponse.json();
+    const result: Data = await response.json();
 
     data.access_token = result.access_token;
     data.token_type = result.token_type;
     data.scope = result.scope;
     data.expires_in = result.expires_in;
 
-    return res.redirect("/");
+    return ctx.redirect("/");
 });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 const host = config.redirect_uri.host ?? "localhost";
 const port = config.redirect_uri.port ?? 8080;
+
 app.listen(port, host, () =>
     console.info(`Listening on http://${host}:${port}`)
 );
